edf5eb7587a21a6c9a6b91307e1eaed0428cee6b
Marco Vermeulen <vermeulen.mp@gmail.com>
2019-07-31 08:23:25 +0100

    Introduce chapter 3 exercises and samples.

---
+++ src/main/kotlin/chapter3/exercises.kt
@@ -0,0 +1,93 @@
+package chapter3
+
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+
+//Exercise 3.1
+fun <A> tail(xs: List<A>): List<A> = when (xs) {
+    is Cons -> xs.tail
+    is Nil -> throw Exception("Nil cannot have a `tail`")
+}
+
+//Exercise 3.2
+fun <A> setHead(xs: List<A>, x: A): List<A> = when (xs) {
+    is Cons -> Cons(x, xs.tail)
+    is Nil -> throw Exception("Cannot replace `head` of a Nil list")
+}
+
+//Exercise 3.3
+fun <A> drop(l: List<A>, n: Int): List<A> =
+        if (n == 0) l
+        else when (l) {
+            is Cons -> drop(l.tail, n - 1)
+            is Nil -> throw Exception("Cannot drop more elements than in list")
+        }
+
+//Exercise 3.4
+fun <A> dropWhile(l: List<A>, f: (A) -> Boolean): List<A> =
+        when (l) {
+            is Cons -> if (f(l.head)) dropWhile(l.tail, f) else l
+            is Nil -> l
+        }
+
+//Exercise 3.5
+fun <A> init(l: List<A>): List<A> =
+        when (l) {
+            is Cons ->
+                if (l.tail == Nil) Nil
+                else Cons(l.head, init(l.tail))
+            is Nil ->
+                throw Exception("Cannot init Nil list")
+        }
+
+//Exercise 3.17
+fun <A, B> map(ss: List<A>, f: (A) -> B): List<B> = TODO()
+
+//Exercise 3.18
+fun <A> filter(ls: List<A>, f: (A) -> Boolean): List<A> = TODO()
+
+//Exercise 3.19
+fun <A, B> flatMap(ss: List<A>, f: (A) -> List<B>): List<B> = TODO()
+
+fun main() {
+    val xs = List.of(1, 2, 3, 4, 5)
+
+    //Exercise 3.1
+    assertThat(tail(xs))
+            .isEqualTo(List.of(2, 3, 4, 5))
+    assertThatThrownBy { tail(Nil) }
+            .isInstanceOf(Exception::class.java)
+            .hasMessage("Nil cannot have a `tail`")
+
+    //Exercise 3.2
+    assertThat(setHead(xs, 6))
+            .isEqualTo(List.of(6, 2, 3, 4, 5))
+    assertThatThrownBy { setHead(Nil, 1) }
+            .isInstanceOf(Exception::class.java)
+            .hasMessage("Cannot replace `head` of a Nil list")
+
+    //Exercise 3.3
+    assertThat(drop(xs, 2))
+            .isEqualTo(List.of(3, 4, 5))
+    assertThat(drop(xs, 5))
+            .isEqualTo(Nil)
+    assertThatThrownBy { drop(xs, 6) }
+            .isInstanceOf(Exception::class.java)
+            .hasMessage("Cannot drop more elements than in list")
+
+    //Exercise 3.4
+    assertThat(dropWhile(xs) { it <= 3 })
+            .isEqualTo(List.of(4, 5))
+    assertThat(dropWhile(xs) { it != 10 })
+            .isEqualTo(Nil)
+
+    //Exercise 3.5
+    assertThat(init(xs))
+            .isEqualTo(List.of(1, 2, 3, 4))
+    assertThat(init(List.of(1)))
+            .isEqualTo(Nil)
+    assertThatThrownBy { init(Nil) }
+            .isInstanceOf(Exception::class.java)
+            .hasMessage("Cannot init Nil list")
+
+}
---
+++ src/main/kotlin/chapter3/samples.kt
@@ -0,0 +1,72 @@
+package chapter3
+
+//List structures
+sealed class List<out A> {
+    companion object {
+        fun sum(ints: List<Int>): Int =
+                when (ints) {
+                    is Nil -> 0
+                    is Cons -> ints.head + sum(ints.tail)
+                }
+
+        fun product(doubles: List<Double>): Double =
+                when (doubles) {
+                    is Nil -> 1.0
+                    is Cons ->
+                        if (doubles.head == 0.0) 0.0
+                        else doubles.head * product(doubles.tail)
+                }
+
+        fun <A> of(vararg aa: A): List<A> {
+            val tail = aa.sliceArray(1 until aa.size)
+            return if (aa.isEmpty()) Nil else Cons(aa[0], of(*tail))
+        }
+
+        fun <A> append(a1: List<A>, a2: List<A>): List<A> =
+                when (a1) {
+                    is Nil -> a2
+                    is Cons -> Cons(a1.head, append(a1.tail, a2))
+                }
+
+        fun <A, B> foldRight(ss: List<A>, z: B, f: (A, B) -> B): B =
+                when (ss) {
+                    is Nil -> z
+                    is Cons -> f(ss.head, foldRight(ss.tail, z, f))
+                }
+
+        fun sum2(ints: List<Int>): Int = foldRight(ints, 0, { a, b -> a + b })
+
+        fun product2(dbs: List<Double>): Double = foldRight(dbs, 1.0, { a, b -> a * b })
+    }
+}
+
+object Nil : List<Nothing>() {
+    override fun toString(): String = "Nil"
+}
+
+data class Cons<out A>(val head: A, val tail: List<A>) : List<A>()
+
+//Tree structures
+sealed class Tree<out A>
+
+data class Leaf<A>(val value: A) : Tree<A>()
+
+data class Branch<A>(val left: Tree<A>, val right: Tree<A>) : Tree<A>()
+
+
+
+fun main() {
+    val ex1: List<Double> = Nil
+    val ex2: List<Int> = Cons(1, Nil)
+    val ex3: List<String> = Cons("a", Cons("b", Nil))
+
+    println(List.sum(Cons(1, Cons(2, Cons(3, Nil)))))
+    println(List.product(Cons(1.0, Cons(2.0, Cons(3.0, Nil)))))
+    println(List.product(Cons(0.0, Cons(2.0, Cons(3.0, Nil)))))
+    println(List.of(1, 2, 3, 4, 5, 6))
+    println(List.append(List.of(1, 2, 3, 4), List.of(5, 6, 7, 8)))
+
+    List.foldRight(Cons(1, Cons(2, Cons(3, Nil))), 0, { x, y -> x + y })
+}
+
+
