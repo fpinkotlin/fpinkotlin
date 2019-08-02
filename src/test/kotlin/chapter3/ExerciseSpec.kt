7278d4324ad330bc45b00d03d283700210c041b9
Marco Vermeulen <vermeulen.mp@gmail.com>
2019-08-02 12:50:25 +0100

    Introduce further exercises 6-9 for chapter 3.

--- src/test/kotlin/chapter3/ExerciseSpec.kt
+++ src/test/kotlin/chapter3/ExerciseSpec.kt
@@ -6,11 +6,9 @@ import io.kotlintest.specs.WordSpec
 
 class ExerciseSpec : WordSpec({
 
-    val xs = List.of(1, 2, 3, 4, 5)
-
     "List.tail" should {
         "return the the tail when present" {
-            tail(xs) shouldBe List.of(2, 3, 4, 5)
+            tail(List.of(1, 2, 3, 4, 5)) shouldBe List.of(2, 3, 4, 5)
         }
 
         "throw an illegal state exception when no tail is present" {
@@ -22,7 +20,7 @@ class ExerciseSpec : WordSpec({
 
     "List.setHead" should {
         "return a new List with a replaced head" {
-            setHead(xs, 6) shouldBe List.of(6, 2, 3, 4, 5)
+            setHead(List.of(1, 2, 3, 4, 5), 6) shouldBe List.of(6, 2, 3, 4, 5)
         }
 
         "throw an illegal state exception when no head is present" {
@@ -34,31 +32,31 @@ class ExerciseSpec : WordSpec({
 
     "List.drop" should {
         "drop a given number of elements within capacity" {
-            drop(xs, 3) shouldBe List.of(4, 5)
+            drop(List.of(1, 2, 3, 4, 5), 3) shouldBe List.of(4, 5)
         }
 
         "drop a given number of elements up to capacity" {
-            drop(xs, 5) shouldBe Nil
+            drop(List.of(1, 2, 3, 4, 5), 5) shouldBe Nil
         }
 
         "throw an illegal state exception when dropped elements exceed capacity" {
             shouldThrow<IllegalStateException> {
-                drop(xs, 6)
+                drop(List.of(1, 2, 3, 4, 5), 6)
             }
         }
     }
 
     "List.dropWhile" should {
         "drop elements until predicate is no longer satisfied" {
-            dropWhile(xs) { it < 4 } shouldBe List.of(4, 5)
+            dropWhile(List.of(1, 2, 3, 4, 5)) { it < 4 } shouldBe List.of(4, 5)
         }
 
         "drop no elements if predicate never satisfied" {
-            dropWhile(xs) { it == 100 } shouldBe List.of(1, 2, 3, 4, 5)
+            dropWhile(List.of(1, 2, 3, 4, 5)) { it == 100 } shouldBe List.of(1, 2, 3, 4, 5)
         }
 
         "drop all elements if predicate always satisfied" {
-            dropWhile(xs) { it < 100 } shouldBe List.of()
+            dropWhile(List.of(1, 2, 3, 4, 5)) { it < 100 } shouldBe List.of()
         }
 
         "return Nil if input is empty" {
@@ -68,7 +66,7 @@ class ExerciseSpec : WordSpec({
 
     "List.init" should {
         "return all but the last element" {
-            init(xs) shouldBe List.of(1, 2, 3, 4)
+            init(List.of(1, 2, 3, 4, 5)) shouldBe List.of(1, 2, 3, 4)
         }
 
         "return Nil if only one element exists" {
@@ -81,5 +79,41 @@ class ExerciseSpec : WordSpec({
             }
         }
     }
+
+    "List.length" should {
+        "calculate the length" {
+            length(List.of(1, 2, 3, 4, 5)) shouldBe 5
+        }
+
+        "calculate zero for an empty list" {
+            length(Nil) shouldBe 0
+        }
+    }
+
+    "List.foldLeft" should {
+        "apply a function f providing a zero accumulator from tail recursive position" {
+            foldLeft(List.of(1, 2, 3, 4, 5), 0, { x, y -> x + y }) shouldBe 15
+        }
+    }
+
+    "List.sumL" should {
+        "add all integers" {
+            sumL(List.of(1, 2, 3, 4, 5)) shouldBe 15
+        }
+    }
+
+    "List.productL" should {
+        "multiply all doubles" {
+            productL(List.of(1.0, 2.0, 3.0, 4.0, 5.0)) shouldBe 120.0
+        }
+    }
+
+    "List.lengthL" should {
+        "count the list elements" {
+            lengthL(List.of(1, 2, 3, 4, 5)) shouldBe 5
+        }
+    }
+
+
 })
 
