8e9d54aa73f79f80c329e474d10865e19917d37f
Marco Vermeulen <vermeulen.mp@gmail.com>
2019-08-13 17:34:04 +0100

    Fixes to chapter 3 solution tags.

--- src/main/kotlin/chapter3/exercises/Solution_3_16.kt
+++ src/main/kotlin/chapter3/exercises/Solution_3_16.kt
@@ -5,8 +5,8 @@ import chapter3.listings.Cons
 import chapter3.listings.List
 
 object Solution_3_16 {
-    // tag::exercise3.16[]
+    // tag::init[]
     fun doubleToString(xs: List<Double>): List<String> =
             foldRight(xs, List.empty(), { d, ds -> Cons(d.toString(), ds) })
-    // end::exercise3.16[]
+    // end::init[]
 }
--- src/main/kotlin/chapter3/exercises/Solution_3_8.kt
+++ src/main/kotlin/chapter3/exercises/Solution_3_8.kt
@@ -4,8 +4,8 @@ import chapter3.exercises.Solution_3_7.foldRight
 import chapter3.listings.List
 
 object Solution_3_8 {
-    // tag::exercise3.8[]
+    // tag::init[]
     fun <A> length(xs: List<A>): Int =
             foldRight(xs, 0, { _, acc -> 1 + acc })
-    // end::exercise3.8[]
+    // end::init[]
 }
