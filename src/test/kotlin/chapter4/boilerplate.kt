d411c32e71fd589de914e0a7e2336541a7a60f11
Marco Vermeulen <vermeulen.mp@gmail.com>
2019-08-23 12:58:43 +0100

    Break association with chapter 4 solutions.

--- src/test/kotlin/chapter4/boilerplate.kt
+++ src/test/kotlin/chapter4/boilerplate.kt
@@ -1,11 +1,10 @@
 package chapter4
 
-import chapter3.exercises.Solution_3_12
 import chapter3.listings.Cons
 import chapter3.listings.List
 import chapter3.listings.Nil
 
-fun <A, B> List<A>.map(f: (A) -> B): List<B> = Solution_3_12.foldRightL(this, List.empty<B>(), { a, b -> Cons(f(a), b) })
+fun <A, B> List<A>.map(f: (A) -> B): List<B> = this.foldRight(List.empty<B>(), { a, b -> Cons(f(a), b) })
 
 fun <A, B> List<A>.foldRight(z: B, f: (A, B) -> B): B =
         when (this) {
@@ -13,11 +12,12 @@ fun <A, B> List<A>.foldRight(z: B, f: (A, B) -> B): B =
             is Cons -> f(this.head, this.tail.foldRight(z, f))
         }
 
-fun List<Double>.sum(): Double = Solution_3_12.foldRightL(this, 0.0, { a, b -> a + b })
 
-fun <A> List<A>.size(): Int = Solution_3_12.foldRightL(this, 0, { _, acc -> 1 + acc })
+fun List<Double>.sum(): Double = this.foldRight(0.0, { a, b -> a + b })
 
-fun List<Double>.isEmpty(): Boolean = when(this) {
+fun <A> List<A>.size(): Int = this.foldRight(0, { _, acc -> 1 + acc })
+
+fun List<Double>.isEmpty(): Boolean = when (this) {
     is Nil -> true
     is Cons -> false
 }
