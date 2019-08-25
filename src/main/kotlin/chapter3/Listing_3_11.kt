9b1f9a20ba42a26df4ce2429f4cd8445a64a8154
Marco Vermeulen <vermeulen.mp@gmail.com>
2019-08-25 22:23:41 +0100

    Further tweaks to chapter 3 exercises tags and formatting.

--- src/main/kotlin/chapter3/Listing_3_11.kt
+++ src/main/kotlin/chapter3/Listing_3_11.kt
@@ -1,5 +1,7 @@
 package chapter3
 
+import chapter3.Listing_3_11.foldRight
+
 object Listing_3_11 {
 
     //tag::init[]
@@ -13,4 +15,18 @@ object Listing_3_11 {
 
     fun product2(dbs: List<Double>): Double = foldRight(dbs, 1.0, { a, b -> a * b })
     //end::init[]
+}
+
+fun main() {
+    //tag::substitution1[]
+    //tag::substitution2[]
+    foldRight(Cons(1, Cons(2, Cons(3, Nil))), 0, { x, y -> x + y })
+    //end::substitution2[]
+    foldRight(Cons(1, Cons(2, Cons(3, Nil))), 0, { x, y -> x + y })
+    1 + foldRight(Cons(2, Cons(3, Nil)), 0, { x, y -> x + y })
+    1 + (2 + foldRight(Cons(3, Nil), 0, { x, y -> x + y }))
+    1 + (2 + (3 + (foldRight(Nil as List<Int>, 0, { x, y -> x + y }))))
+    1 + (2 + (3 + (0)))
+    6
+    //end::substitution1[]
 }
--- src/main/kotlin/chapter3/Listing_3_7.kt
+++ src/main/kotlin/chapter3/Listing_3_7.kt
@@ -2,9 +2,9 @@ package chapter3
 
 object Listing_3_7 {
     //tag::init[]
-    fun sum1(xs: List<Int>): Int = when(xs) {
+    fun sum(xs: List<Int>): Int = when(xs) {
         is Nil -> 0
-        is Cons -> xs.head + sum1(xs.tail) // <1>
+        is Cons -> xs.head + sum(xs.tail) // <1>
     }
     //end::init[]
 }
