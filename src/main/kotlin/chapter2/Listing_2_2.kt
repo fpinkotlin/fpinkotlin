ef2f43d9e7b5bda0010af1ba3489b3f21e8b3ba3
Marco Vermeulen <vermeulen.mp@gmail.com>
2019-09-09 18:10:06 +0100

    Small tweaks to Listing 2.2

--- src/main/kotlin/chapter2/Listing_2_2.kt
+++ src/main/kotlin/chapter2/Listing_2_2.kt
@@ -16,9 +16,9 @@ object Listing_2_2_1 {
 
     //tag::factorial2[]
     fun factorial(i: Int): Int {
-        tailrec fun go(n: Int, acc: Int): Int =
+        tailrec fun go(n: Int, acc: Int): Int = // <1>
                 if (n <= 0) acc
-                else go(n - 1, n * acc)
+                else go(n - 1, n * acc) // <2>
         return go(i, 1)
     }
     //end::factorial2[]
