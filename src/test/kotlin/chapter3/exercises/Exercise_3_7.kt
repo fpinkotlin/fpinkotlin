c08191b9cfbea6ce10a46d86674fcd16bd8ee225
Marco Vermeulen <vermeulen.mp@gmail.com>
2019-11-09 22:49:50 +0000

    Improve trace listing for exercise 3.7.

--- src/test/kotlin/chapter3/exercises/Exercise_3_7.kt
+++ src/test/kotlin/chapter3/exercises/Exercise_3_7.kt
@@ -15,12 +15,13 @@ object Exercise_3_7 {
     val f = { x: Int, y: List<Int> -> Cons(x, y) }
     val z = Nil as List<Int>
 
-    val step1 = foldRight(List.of(1, 2, 3), z, f)
-    val step2 = Cons(1, foldRight(List.of(2, 3), z, f))
-    val step3 = Cons(1, Cons(2, foldRight(List.of(3), z, f)))
-    val step4 = Cons(1, Cons(2, Cons(3, foldRight(List.empty(), z, f))))
-    val step5: List<Int> = Cons(1, Cons(2, Cons(3, Nil)))
-
+    val trace = {
+        foldRight(List.of(1, 2, 3), z, f)
+        Cons(1, foldRight(List.of(2, 3), z, f))
+        Cons(1, Cons(2, foldRight(List.of(3), z, f)))
+        Cons(1, Cons(2, Cons(3, foldRight(List.empty(), z, f))))
+        Cons(1, Cons(2, Cons(3, Nil)))
+    }
 }
 
 fun main() {
--- src/test/kotlin/chapter3/solutions/Solution_3_7.kt
+++ src/test/kotlin/chapter3/solutions/Solution_3_7.kt
@@ -14,9 +14,11 @@ fun <A, B> foldRight(xs: List<A>, z: B, f: (A, B) -> B): B =
 val f = { x: Int, y: List<Int> -> Cons(x, y) }
 val z = Nil as List<Int>
 
-val step1 = foldRight(List.of(1, 2, 3), z, f)
-val step2 = Cons(1, foldRight(List.of(2, 3), z, f))
-val step3 = Cons(1, Cons(2, foldRight(List.of(3), z, f)))
-val step4 = Cons(1, Cons(2, Cons(3, foldRight(List.empty(), z, f))))
-val step5: List<Int> = Cons(1, Cons(2, Cons(3, Nil)))
+val trace = {
+    foldRight(List.of(1, 2, 3), z, f)
+    Cons(1, foldRight(List.of(2, 3), z, f))
+    Cons(1, Cons(2, foldRight(List.of(3), z, f)))
+    Cons(1, Cons(2, Cons(3, foldRight(List.empty(), z, f))))
+    Cons(1, Cons(2, Cons(3, Nil)))
+}
 // end::init[]
