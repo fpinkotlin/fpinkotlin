f4b06cea0cf0ea2a4889f925db12f99ec5574ef2
Marco Vermeulen <vermeulen.mp@gmail.com>
2019-11-19 20:42:33 +0000

    Tweaks on chapter 2 code listings.

--- src/main/kotlin/chapter2/Listing_2_4.kt
+++ src/main/kotlin/chapter2/Listing_2_4.kt
@@ -5,9 +5,11 @@ object Listing_2_4 {
     //tag::init[]
     fun findFirst(ss: Array<String>, key: String): Int {
         tailrec fun loop(n: Int): Int =
-            if (n >= ss.size) -1 // <1>
-            else if (ss[n] == key) n // <2>
-            else loop(n + 1) // <3>
+            when {
+                n >= ss.size -> -1 // <1>
+                ss[n] == key -> n // <2>
+                else -> loop(n + 1) // <3>
+            }
         return loop(0) // <4>
     }
     //end::init[]
