da820d9064e23b2f1361626fc93d8a282361a997
Marco Vermeulen <vermeulen.mp@gmail.com>
2020-03-05 20:20:08 +0000

    Tweaks to chapter 8 code.

--- src/test/kotlin/chapter8/exercises/ex12/listing.kt
+++ src/test/kotlin/chapter8/exercises/ex12/listing.kt
@@ -6,12 +6,6 @@ import chapter8.State
 data class SGen<A>(val forSize: (Int) -> Gen<A>)
 
 data class Gen<A>(val sample: State<RNG, A>) {
-
-    companion object {
-        fun <A> listOfN(n: Int, ga: Gen<A>): Gen<List<A>> =
-            Gen(State.sequence(List(n) { ga.sample }))
-    }
-
     //tag::init[]
     fun listOf(): SGen<List<A>> = TODO()
     //end::init[]
