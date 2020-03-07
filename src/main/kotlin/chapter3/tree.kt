2ba25fee3a0de556d574e2ee988a15d055a3b219
Marco Vermeulen <vermeulen.mp@gmail.com>
2020-03-07 13:07:43 +0000

    Code formatting for chapter 3 listings.

--- src/main/kotlin/chapter3/tree.kt
+++ src/main/kotlin/chapter3/tree.kt
@@ -6,5 +6,8 @@ sealed class Tree<out A>
 
 data class Leaf<A>(val value: A) : Tree<A>()
 
-data class Branch<A>(val left: Tree<A>, val right: Tree<A>) : Tree<A>()
+data class Branch<A>(
+    val left: Tree<A>,
+    val right: Tree<A>
+) : Tree<A>()
 //end::init[]
