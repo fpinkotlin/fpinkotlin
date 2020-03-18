c705efc6e4db34ae43fa6279f829053a27999a66
Marco Vermeulen <vermeulen.mp@gmail.com>
2020-03-18 22:24:29 +0000

    Apply linter to Exercise 3.13.

--- src/test/kotlin/chapter3/exercises/Exercise_3_13.kt
+++ src/test/kotlin/chapter3/exercises/Exercise_3_13.kt
@@ -7,18 +7,25 @@ import io.kotlintest.specs.WordSpec
 // tag::init[]
 fun <A> append(a1: List<A>, a2: List<A>): List<A> = TODO()
 // end::init[]
+
 fun <A> appendL(a1: List<A>, a2: List<A>): List<A> = TODO()
 
 class Exercise_3_13 : WordSpec({
     "!list append" should {
         "append two lists to each other using foldRight" {
-            append(List.of(1, 2, 3), List.of(4, 5, 6)) shouldBe List.of(1, 2, 3, 4, 5, 6)
+            append(
+                List.of(1, 2, 3),
+                List.of(4, 5, 6)
+            ) shouldBe List.of(1, 2, 3, 4, 5, 6)
         }
     }
 
     "!list appendL" should {
         "append two lists to each other using foldLeft" {
-            appendL(List.of(1, 2, 3), List.of(4, 5, 6)) shouldBe List.of(1, 2, 3, 4, 5, 6)
+            appendL(
+                List.of(1, 2, 3),
+                List.of(4, 5, 6)
+            ) shouldBe List.of(1, 2, 3, 4, 5, 6)
         }
     }
 })
--- src/test/kotlin/chapter3/solutions/Solution_3_13.kt
+++ src/test/kotlin/chapter3/solutions/Solution_3_13.kt
@@ -9,18 +9,25 @@ import io.kotlintest.specs.WordSpec
 fun <A> append(a1: List<A>, a2: List<A>): List<A> =
     foldRight(a1, a2, { x, y -> Cons(x, y) })
 // end::init[]
+
 fun <A> appendL(a1: List<A>, a2: List<A>): List<A> =
     foldLeft(reverse(a1), a2, { y, x -> Cons(x, y) })
 
 class Solution_3_13 : WordSpec({
     "list append" should {
         "append two lists to each other using foldRight" {
-            append(List.of(1, 2, 3), List.of(4, 5, 6)) shouldBe List.of(1, 2, 3, 4, 5, 6)
+            append(
+                List.of(1, 2, 3),
+                List.of(4, 5, 6)
+            ) shouldBe List.of(1, 2, 3, 4, 5, 6)
         }
     }
     "list appendL" should {
         "append two lists to each other using foldLeft" {
-            chapter3.exercises.appendL(List.of(1, 2, 3), List.of(4, 5, 6)) shouldBe List.of(1, 2, 3, 4, 5, 6)
+            appendL(
+                List.of(1, 2, 3),
+                List.of(4, 5, 6)
+            ) shouldBe List.of(1, 2, 3, 4, 5, 6)
         }
     }
 })
