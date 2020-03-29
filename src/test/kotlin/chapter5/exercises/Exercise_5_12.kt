bdf6433897374ccd0d931715eba9ca628c79198a
Jean-François Morin <ioannes.franciscus@gmail.com>
2020-03-29 00:59:52 -0400

    Added missing imports so that tests pass with a correct solution without depending on future exercises

--- src/test/kotlin/chapter5/exercises/Exercise_5_12.kt
+++ src/test/kotlin/chapter5/exercises/Exercise_5_12.kt
@@ -2,6 +2,7 @@ package chapter5.exercises
 
 import chapter3.List
 import chapter5.Stream
+import chapter5.solutions.take
 import chapter5.solutions.toList
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
