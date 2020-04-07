2dc95a1f09b9c6f0c9f40812d5de8341f793a068
Marco Vermeulen <vermeulen.mp@gmail.com>
2020-04-07 19:32:28 +0100

    Improve solution for chapter 9 ex 14.

--- src/test/kotlin/chapter9/solutions/ex14/listing.kt
+++ src/test/kotlin/chapter9/solutions/ex14/listing.kt
@@ -1,6 +1,7 @@
 package chapter9.solutions.ex14
 
 import arrow.core.Option
+import arrow.core.getOrElse
 import arrow.core.lastOrNone
 import chapter9.solutions.final.Location
 
@@ -15,8 +16,50 @@ data class ParseError(val stack: List<Pair<Location, String>> = emptyList()) {
             .map { Pair(it, s) }
             .toList())
 
-    fun latest(): Option<Pair<Location, String>> = stack.lastOrNone()
+    private fun latest(): Option<Pair<Location, String>> = stack.lastOrNone()
 
-    fun latestLoc(): Option<Location> = latest().map { it.first }
+    private fun latestLoc(): Option<Location> = latest().map { it.first }
+
+    /**
+     * Display collapsed error stack - any adjacent stack elements with the
+     * same location are combined on one line. For the bottommost error, we
+     * display the full line, with a caret pointing to the column of the
+     * error.
+     * Example:
+     * 1.1 file 'companies.json'; array
+     * 5.1 object
+     * 5.2 key-value
+     * 5.10 ':'
+     * { "MSFT" ; 24,
+     *          ^
+     */
+    override fun toString(): String =
+        if (stack.isEmpty()) "no error message"
+        else {
+            val collapsed = collapseStack(stack)
+            val context =
+                collapsed.lastOrNone()
+                    .map { "\n\n" + it.first.line }
+                    .getOrElse { "" } +
+                    collapsed.lastOrNone()
+                        .map { "\n" + it.first.col }
+                        .getOrElse { "" }
+
+            collapsed.joinToString { (loc, msg) ->
+                "${loc.line}.${loc.col} $msg"
+            } + context
+        }
+
+    /* Builds a collapsed version of the given error stack -
+     * messages at the same location have their messages merged,
+     * separated by semicolons.
+     */
+    private fun collapseStack(
+        stk: List<Pair<Location, String>>
+    ): List<Pair<Location, String>> =
+        stk.groupBy { it.first }
+            .mapValues { it.value.joinToString() }
+            .toList()
+            .sortedBy { it.first.offset }
 }
 //end::init[]
