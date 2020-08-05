c5d3dbd1e1210d5640c552dd73f8aaaf05e27fd7
Marco Vermeulen <vermeulen.mp@gmail.com>
2020-08-05 21:38:02 +0100

    Provide applicative for exercise 12.19.

--- src/test/kotlin/chapter12/exercises/ex19/listing.kt
+++ src/test/kotlin/chapter12/exercises/ex19/listing.kt
@@ -1,9 +1,14 @@
 package chapter12.exercises.ex19
 
+import arrow.Kind
 import chapter11.Monad
 import chapter12.Applicative
 import chapter12.CompositePartialOf
 import chapter12.Traversable
+
+fun <F> applicative() = object : Applicative<F> {
+    override fun <A> unit(a: A): Kind<F, A> = TODO()
+}
 
 //tag::init[]
 fun <G, H, A> composeM(
