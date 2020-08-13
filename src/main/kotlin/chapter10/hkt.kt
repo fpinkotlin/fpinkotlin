bb72fa80de39812bd64422b0c9520b20d47e668a
Marco Vermeulen <vermeulen.mp@gmail.com>
2020-08-13 21:28:23 +0100

    Update higher kinded types sidebar.

--- src/main/kotlin/chapter10/hkt.kt
+++ src/main/kotlin/chapter10/hkt.kt
@@ -1,7 +1,9 @@
 package chapter10
 
 import arrow.Kind
+import arrow.Kind2
 import arrow.higherkind
+import chapter11.Monad
 
 /*
 //tag::init1[]
@@ -10,7 +12,7 @@ interface Foldable<F<A>> {
 }
 
 object ListFoldable : Foldable<List<A>> {
-    //some code
+    //some method implementations with parametrized A
 }
 //end::init1[]
 */
@@ -77,18 +79,31 @@ data class Cons<out A>(
 fun kotlin.collections.List<Int>.asConsList(): List<Int> =
     List.of(*this.toTypedArray())
 
+interface FoldableListing1 {
+    //tag::init45[]
+    fun <A, B> foldRight(fa: Kind<ForList, A>, z: B, f: (A, B) -> B): B
+    //end::init45[]
+        = TODO()
+}
+
+interface FoldableListing2 {
+    //tag::init46[]
+    fun <A, B> foldRight(fa: ListOf<A>, z: B, f: (A, B) -> B): B
+    //end::init46[]
+        = TODO()
+}
+
 //tag::init5[]
 object ListFoldable : Foldable<ForList> {
+
     //tag::foldright[]
     override fun <A, B> foldRight(
         fa: ListOf<A>,
         z: B,
         f: (A, B) -> B
     ): B =
-    //end::foldright[]
-        //tag::foldrightimpl[]
         fa.fix().foldRight(z, f)
-    //end::foldrightimpl[]
+    //end::foldright[]
 }
 //end::init5[]
 
@@ -104,17 +119,55 @@ fun <A> ListOf<A>.fix() = this as List<A>
 
 //tag::init8[]
 @higherkind
-sealed class ListHK<out A> {
+sealed class ListK<out A> : ListKOf<A> {
     fun <B> foldRight(z: B, f: (A, B) -> B): B = TODO()
 }
 //end::init8[]
 
 //tag::init9[]
-object ListHKFoldable : Foldable<ForListHK> {
+object ListKFoldable : Foldable<ForListK> {
     override fun <A, B> foldRight(
-        fa: ListHKOf<A>,
+        fa: ListKOf<A>,
         z: B,
         f: (A, B) -> B
     ): B = fa.fix().foldRight(z, f)
 }
 //end::init9[]
+
+data class State<S, out A>(val run: (S) -> Pair<A, S>) : StateOf<S, A> {
+    fun <B> flatMap(f: (A) -> State<S, B>): State<S, B> = TODO()
+}
+
+fun <S, A> StateOf<S, A>.fix() = this as State<S, A>
+
+//tag::init10[]
+sealed class ForState private constructor() {
+    companion object
+}
+
+typealias StateOf<S, A> = Kind2<ForState, S, A>
+//end::init10[]
+
+//tag::init11[]
+typealias StatePartialOf<S> = Kind<ForState, S>
+//end::init11[]
+
+//tag::init12[]
+interface StateMonad<S> : Monad<StatePartialOf<S>> { // <1>
+
+    override fun <A> unit(a: A): StateOf<S, A> = // <2>
+        State { s -> Pair(a, s) }
+
+    override fun <A, B> flatMap(
+        fa: StateOf<S, A>, // <2>
+        f: (A) -> StateOf<S, B>
+    ): StateOf<S, B> =
+        fa.fix().flatMap { a -> f(a).fix() } // <3>
+}
+//end::init12[]
+
+//tag::init13[]
+val intStateMonad: StateMonad<Int> = object : StateMonad<Int> {}
+
+val stringStateMonad: StateMonad<String> = object : StateMonad<String> {}
+//end::init13[]
