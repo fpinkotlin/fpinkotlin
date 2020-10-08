23241e56f8b702e8e9d8be3fd570f2be023c7487
Marco Vermeulen <vermeulen.mp@gmail.com>
2020-10-08 21:40:43 +0100

    Add section 13.3.2 code for Tailrec.

---
+++ src/main/kotlin/chapter13/sec3_2/listing.kt
@@ -0,0 +1,34 @@
+package chapter13.sec3_2
+
+import chapter13.sec3_1.IO
+import chapter13.sec3_1.Return
+import chapter13.sec3_1.Suspend
+
+object Listing {
+
+    //tag::init1[]
+    val f = { x: Int -> x }
+    val g = List(100000) { idx -> f } // <1>
+        .fold(f) { ff, h ->
+            { n: Int ->
+                ff(h(n))
+            }
+        } // <2>
+    //end::init1[]
+
+    val result = g(42)
+}
+
+object Listing2 {
+    //tag::init2[]
+    val f = { x: Int -> Return(x) }
+    val g = List(100000) { idx -> f } // <1>
+        .fold(f) { a: (Int) -> IO<Int>, b: (Int) -> IO<Int> ->
+            { x: Int ->
+                Suspend { Unit }.flatMap { _ ->
+                    a(x).flatMap(b)
+                }
+            }
+        } // <2>
+    //end::init2[]
+}
---
+++ src/main/kotlin/chapter13/sec3_2/listing2.kt
@@ -0,0 +1,30 @@
+package chapter13.sec3_2
+
+import arrow.Kind
+
+class ForTailrec {
+    companion object
+}
+
+typealias TailrecOf<A> = Kind<ForTailrec, A>
+
+inline fun <A> TailrecOf<A>.fix(): Tailrec<A> = this as Tailrec<A>
+
+//tag::init1[]
+sealed class Tailrec<A> : TailrecOf<A> {
+
+    companion object {
+        fun <A> unit(a: A) = Suspend { a }
+    }
+
+    fun <B> flatMap(f: (A) -> Tailrec<B>): Tailrec<B> = FlatMap(this, f)
+    fun <B> map(f: (A) -> B): Tailrec<B> = flatMap { a -> Return(f(a)) }
+}
+
+data class Return<A>(val a: A) : Tailrec<A>()
+data class Suspend<A>(val r: () -> A) : Tailrec<A>()
+data class FlatMap<A, B>(
+    val sub: Tailrec<A>,
+    val f: (A) -> Tailrec<B>
+) : Tailrec<B>()
+//end::init1[]
