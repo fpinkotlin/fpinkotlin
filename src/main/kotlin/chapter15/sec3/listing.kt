7bcfbc77b00b2be55b188315b70e7482003db0ba
Marco Vermeulen <vermeulen.mp@gmail.com>
2020-12-13 10:23:52 +0000

    Add section 15.3.1 code along with exercise 15.10.

--- src/main/kotlin/chapter15/sec3/listing.kt
+++ src/main/kotlin/chapter15/sec3/listing.kt
@@ -4,8 +4,20 @@ import arrow.Kind
 import arrow.core.andThen
 import arrow.higherkind
 import chapter12.Either
+import chapter12.Left
+import chapter12.Right
+import chapter12.fix
+import chapter13.boilerplate.io.ForIO
+import chapter13.boilerplate.io.IO
+import chapter13.boilerplate.io.IOOf
+import chapter13.boilerplate.io.fix
 import chapter15.sec3.Process.Companion.Await
+import chapter15.sec3.Process.Companion.Emit
+import chapter15.sec3.Process.Companion.End
 import chapter15.sec3.Process.Companion.Halt
+import java.io.BufferedReader
+import java.io.FileReader
+import java.util.concurrent.ExecutorService
 
 //tag::init1[]
 @higherkind
@@ -57,14 +69,23 @@ sealed class Process<F, O> : ProcessOf<F, O> {
                 Try { f(head) }.apply { tail.flatMap(f) }
             is Await<*, *, *> -> {
                 await<F, O, O2>(
-                    this.req,
-                    this.recv
+                    req,
+                    recv
                 ) { it.flatMap(f) }
             }
         }
+
     //end::init5[]
     //tag::ignore[]
-    //TODO: implement `map` and `filter`
+    fun <O2> map(f: (O) -> O2): Process<F, O2> =
+        when (this) {
+            is Await<*, *, *> ->
+                await<F, O, O2>(req, recv) { it.map(f) }
+            is Emit ->
+                Try { Emit(f(head), tail.map(f)) }
+            is Halt ->
+                Halt(err)
+        }
     //end::ignore[]
 }
 
@@ -89,3 +110,94 @@ fun <F, A, O> await(
         recv as (Either<Throwable, A>) -> Process<F, A> andThen fn
     ).fix()
 //end::init4[]
+
+private fun <A> unsafePerformIO(
+    ioa: IOOf<A>,
+    pool: ExecutorService
+): A = ioa.fix().run()
+
+/*
+//tag::init6[]
+data class Await<ForIO, A, O>(
+    val req: IO<A>,
+    val recv: (Either<Throwable, A>) -> Process<ForIO, O>
+) : Process<ForIO, O>()
+//end::init6[]
+ */
+
+//tag::init7[]
+fun <O> runLog(src: Process<ForIO, O>): IO<Sequence<O>> = IO {
+
+    val E = java.util.concurrent.Executors.newFixedThreadPool(4)
+
+    tailrec fun go(cur: Process<ForIO, O>, acc: Sequence<O>): Sequence<O> =
+        when (cur) {
+            is Emit ->
+                go(cur.tail, acc + cur.head)
+            is Halt ->
+                when (val e = cur.err) {
+                    is End -> acc
+                    else -> throw e
+                }
+            is Await<*, *, *> -> {
+                val re = cur.req as IO<O>
+                val rcv = cur.recv
+                    as (Either<Throwable, O>) -> Process<ForIO, O>
+                val next: Process<ForIO, O> = try {
+                    rcv(Right(unsafePerformIO(re, E)).fix())
+                } catch (err: Throwable) {
+                    rcv(Left(err))
+                }
+                go(next, acc)
+            }
+        }
+    try {
+        go(src, emptySequence())
+    } finally {
+        E.shutdown()
+    }
+}
+//end::init7[]
+
+fun <F, A, O> await(
+    req: Kind<Any?, Any?>,
+    recv: (Either<Throwable, Nothing>) -> Process<out Any?, out Any?>
+): Process<F, O> =
+    Await(
+        req as Kind<F, Nothing>,
+        recv as (Either<Throwable, A>) -> Process<F, A>
+    ).fix()
+
+const val fileName = "src/main/resources/samples/lines.txt"
+
+//tag::init8[]
+val p: Process<ForIO, String> =
+    await<ForIO, BufferedReader, String>(
+        IO { BufferedReader(FileReader(fileName)) }
+    ) { ei1: Either<Throwable, BufferedReader> ->
+        when (ei1) {
+            is Right -> processNext(ei1)
+            is Left -> Halt(ei1.value) // <1>
+        }
+    }
+
+private fun processNext(ei1: Right<BufferedReader>): Process<ForIO, String> =
+    await<ForIO, BufferedReader, String>(
+        IO { ei1.value.readLine() }
+    ) { ei2: Either<Throwable, String?> ->
+        when (ei2) {
+            is Right ->
+                if (ei2.value == null) Halt(End) // <2>
+                else Emit(ei2.value, processNext(ei1))
+            is Left -> {
+                await<ForIO, Nothing, Nothing>(
+                    IO { ei1.value.close() }
+                ) { Halt(ei2.value) } // <3>
+            }
+        }
+    }
+//end::init8[]
+
+fun main() {
+    runLog(p).run().forEach { println(it) }
+}
---
+++ src/test/kotlin/chapter15/solutions/ex10/listing.kt
@@ -0,0 +1,42 @@
+package chapter15.solutions.ex10
+
+import arrow.Kind
+import chapter12.Either
+import chapter13.Monad
+import chapter15.sec3.Process
+import chapter15.sec3.Process.Companion.Await
+import chapter15.sec3.Process.Companion.Emit
+import chapter15.sec3.Process.Companion.End
+import chapter15.sec3.Process.Companion.Halt
+import chapter15.sec3.Try
+
+//tag::init[]
+fun <F, O> Process<F, O>.runLog(MC: MonadCatch<F>): Kind<F, Sequence<O>> {
+
+    fun go(cur: Process<F, O>, acc: Sequence<O>): Kind<F, Sequence<O>> =
+        when (cur) {
+            is Emit ->
+                go(cur.tail, acc + cur.head)
+            is Halt ->
+                when (val e = cur.err) {
+                    is End -> MC.unit(acc)
+                    else -> throw e
+                }
+            is Await<*, *, *> -> {
+                val re: Kind<F, O> = cur.req as Kind<F, O>
+                val rcv: (Either<Throwable, O>) -> Process<F, O> =
+                    cur.recv as (Either<Throwable, O>) -> Process<F, O>
+                MC.flatMap(MC.attempt(re)) { ei ->
+                    go(Try { rcv(ei) }, acc)
+                }
+            }
+        }
+
+    return go(this, emptySequence())
+}
+
+interface MonadCatch<F> : Monad<F> {
+    fun <A> attempt(a: Kind<F, A>): Kind<F, Either<Throwable, A>>
+    fun <A> fail(t: Throwable): Kind<F, A>
+}
+//end::init[]
