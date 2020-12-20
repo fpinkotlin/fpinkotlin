8fc32263d9186309d96a847cc7780c97362167f2
Marco Vermeulen <vermeulen.mp@gmail.com>
2020-12-20 16:35:07 +0000

    Add code samples for section 15.3.7.

---
+++ src/main/kotlin/chapter15/sec3_7/listing.kt
@@ -0,0 +1,44 @@
+package chapter15.sec3_7
+
+import chapter13.boilerplate.io.ForIO
+import chapter15.sec3_3.Process
+import chapter15.sec3_5.fileW
+import chapter15.sec3_5.lines
+import chapter15.sec3_5.to
+
+fun fahrenheitToCelsius(f: Double): Double = (f - 32) * 5.0 / 9.0
+
+//tag::init1[]
+val convertAll: Process<ForIO, Unit> =
+    fileW("celsius.tx").take(1).flatMap { out ->
+        lines("fahrenheits.txt")
+            .flatMap { infile ->
+                lines(infile)
+                    .map { fahrenheitToCelsius(it.toDouble()) }
+                    .flatMap { out(it.toString()) }
+            }
+    }.drain()
+//end::init1[]
+
+//tag::init2[]
+val convertMultiSink: Process<ForIO, Unit> =
+    lines("fahrenheits.txt")
+        .flatMap { infile ->
+            lines(infile)
+                .map { fahrenheitToCelsius(it.toDouble()) }
+                .map { it.toString() }
+                .to(fileW("${infile}.celsius"))
+        }.drain()
+//end::init2[]
+
+//tag::init3[]
+val convertMultisink2: Process<ForIO, Unit> =
+    lines("fahrenheits.txt").flatMap { infile ->
+        lines(infile)
+            .filter { !it.startsWith("#") }
+            .map { fahrenheitToCelsius(it.toDouble()) }
+            .filter { it > 0 } // discard temperatures below zero
+            .map { it.toString() }
+            .to(fileW("${infile}.celsius"))
+    }.drain()
+//end::init3[]
