c5b4540d711b9a5eac1c62e7848c740a5e17335f
Marco Vermeulen <vermeulen.mp@gmail.com>
2020-12-20 13:18:04 +0000

    Add code samples for section 15.3.6.

---
+++ src/main/kotlin/chapter15/sec3_6/listing.kt
@@ -0,0 +1,29 @@
+package chapter15.sec3_6
+
+import chapter13.boilerplate.io.ForIO
+import chapter13.boilerplate.io.IO
+import chapter15.sec3_3.Process
+import chapter15.sec3_5.join
+import chapter15.sec3_5.zipWith
+import java.sql.Connection
+import java.sql.PreparedStatement
+
+//tag::init1[]
+fun <F, O, O2> Process<F, O>.through(
+    p2: Process<F, (O) -> Process<F, O2>>
+): Process<F, O2> =
+    join(
+        zipWith(this, p2) { o: O, f: (O) -> Process<F, O2> -> f(o) }
+    )
+//end::init1[]
+
+//tag::init2[]
+typealias Channel<F, I, O> = Process<F, (I) -> Process<F, O>>
+//end::init2[]
+
+//tag::init3[]
+fun query(
+    conn: IO<Connection>
+): Channel<ForIO, (Connection) -> PreparedStatement, Map<String, Any>>
+//end::init3[]
+= TODO()
