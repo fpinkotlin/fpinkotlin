bcc999ccd3a0e47e8d4b7e8f050e0231dbcaf5f6
Marco Vermeulen <vermeulen.mp@gmail.com>
2021-03-25 16:42:37 +0000

    Move chapter 4 listings to standard file layout.

--- src/main/kotlin/chapter4/either.kt
+++ src/main/kotlin/chapter4/either.kt
@@ -4,5 +4,6 @@ package chapter4
 sealed class Either<out E, out A>
 
 data class Left<out E>(val value: E) : Either<E, Nothing>()
+
 data class Right<out A>(val value: A) : Either<Nothing, A>()
 //end::init[]
