cca850302b9f667913b9b87e7adb95c1fc55df78
Marco Vermeulen <vermeulen.mp@gmail.com>
2025-03-22 09:54:00 +0000

    Fix ktlint issues and downgrade Kotlin version to 1.3.21 for compatibility with Arrow 0.10.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,32 +2,37 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("org.jlleitschuh.gradle.ktlint") version "9.2.1"
-    kotlin("jvm") version "1.6.0"
+    kotlin("jvm") version "1.3.21"
+    kotlin("kapt") version "1.3.21"
 }
 
 val test by tasks.getting(Test::class) {
     useJUnitPlatform { }
 }
 
-val arrowVersion = "1.0.1"
+val arrowVersion = "0.10.2"
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("io.arrow-kt:arrow-core:$arrowVersion")
-    implementation("io.arrow-kt:arrow-fx-coroutines:$arrowVersion")
-    implementation("io.arrow-kt:arrow-fx-stm:$arrowVersion")
-    implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.5")
+    implementation("io.arrow-kt:arrow-core-data:$arrowVersion")
+    implementation("io.arrow-kt:arrow-fx:$arrowVersion")
+    implementation("io.arrow-kt:arrow-mtl:$arrowVersion")
+    implementation("io.arrow-kt:arrow-syntax:$arrowVersion")
+    implementation(
+        "org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.5"
+    )
     implementation("io.github.microutils:kotlin-logging:1.7.8")
     implementation("org.awaitility:awaitility:4.0.2")
     runtimeOnly("org.slf4j:slf4j-simple:1.7.28")
 
     // need this at compile level for chapter 8
     implementation("io.kotlintest:kotlintest-runner-junit5:3.3.2")
-    // Arrow 1.0.1 doesn't use kapt anymore
+    kapt("io.arrow-kt:arrow-meta:$arrowVersion")
 }
 
 repositories {
     mavenCentral()
-    maven("https://repo.kotlin.link") // Replacement for Bintray Kotlin repositories
+    // Replacement for Bintray Kotlin repositories
+    maven("https://repo.kotlin.link")
 }
 
 tasks.withType<KotlinCompile>().configureEach {
@@ -44,4 +49,8 @@ ktlint {
             "no-line-break-before-assignment"
         )
     )
+}
+
+kapt {
+    useBuildCache = false
 }
