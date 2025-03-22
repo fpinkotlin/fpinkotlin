78fe631380b0b169f25992700746336df55fb0ac
Marco Vermeulen <vermeulen.mp@gmail.com>
2025-03-22 09:49:37 +0000

    Update Arrow version to 1.0.1 and remove kapt plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,20 +3,18 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.jlleitschuh.gradle.ktlint") version "9.2.1"
     kotlin("jvm") version "1.6.0"
-    kotlin("kapt") version "1.6.0"
 }
 
 val test by tasks.getting(Test::class) {
     useJUnitPlatform { }
 }
 
-val arrowVersion = "0.10.2"
+val arrowVersion = "1.0.1"
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("io.arrow-kt:arrow-core-data:$arrowVersion")
-    implementation("io.arrow-kt:arrow-fx:$arrowVersion")
-    implementation("io.arrow-kt:arrow-mtl:$arrowVersion")
-    implementation("io.arrow-kt:arrow-syntax:$arrowVersion")
+    implementation("io.arrow-kt:arrow-core:$arrowVersion")
+    implementation("io.arrow-kt:arrow-fx-coroutines:$arrowVersion")
+    implementation("io.arrow-kt:arrow-fx-stm:$arrowVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.5")
     implementation("io.github.microutils:kotlin-logging:1.7.8")
     implementation("org.awaitility:awaitility:4.0.2")
@@ -24,7 +22,7 @@ dependencies {
 
     // need this at compile level for chapter 8
     implementation("io.kotlintest:kotlintest-runner-junit5:3.3.2")
-    kapt("io.arrow-kt:arrow-meta:$arrowVersion")
+    // Arrow 1.0.1 doesn't use kapt anymore
 }
 
 repositories {
@@ -46,8 +44,4 @@ ktlint {
             "no-line-break-before-assignment"
         )
     )
-}
-
-kapt {
-    useBuildCache = false
 }
